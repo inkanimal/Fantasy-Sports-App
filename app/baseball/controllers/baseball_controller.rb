ee40a34b01eb036943f993867327e08c20612840
inkanimal <theatomproject@gmail.com>
2018-09-15 11:20:05 -0400

    baseball controller more paths added

--- app/baseball/controllers/baseball_controller.rb
+++ app/baseball/controllers/baseball_controller.rb
@@ -33,7 +33,7 @@ class BaseballController < Sinatra::Base
       user = User.new(username: params["username"], email: params["email"], password: params["password"])
       if  user.save
         session[:user_id] = user.id
-        redirect "/home"
+        redirect "/create_team"
       end
      end
    end
@@ -50,7 +50,7 @@ class BaseballController < Sinatra::Base
      user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
        session[:user_id] = user.id
-       redirect to '/home'
+       redirect to '/create_team'
       else
        redirect to '/login'
       end
@@ -63,6 +63,43 @@ class BaseballController < Sinatra::Base
      else
       redirect to '/'
      end
+   end
+
+   get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :"/users/show"
+   end
+
+   get '/tweets' do
+     if logged_in?
+       @user = current_user
+       @tweets = Tweet.all
+       erb :'tweets/tweets'
+    else
+      redirect 'users/login'
+    end
+   end
+
+   get '/teams/new' do
+     if !logged_in?
+       redirect 'users/login'
+     else
+       @user = current_user
+       erb :"teams/create_team"
+     end
+   end
+
+   post '/teams' do
+    if logged_in?
+     if params[:team_name].empty?
+        redirect '/teams/new'
+      end
+       @user = current_user
+       new_team = Team.new(team_name: params[:team_name], user: current_user)
+     if new_team.save
+       redirect  '/home'
+     end
+    end
    end
 
 
