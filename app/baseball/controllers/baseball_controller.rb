42421be58835fc9ee8a457abd68268e9cb51b9da
inkanimal <theatomproject@gmail.com>
2018-09-15 10:59:56 -0400

    renamed views

--- app/baseball/controllers/baseball_controller.rb
+++ app/baseball/controllers/baseball_controller.rb
@@ -38,5 +38,32 @@ class BaseballController < Sinatra::Base
      end
    end
 
+   get '/login' do
+    if logged_in?
+     redirect to '/home'
+    else
+     erb :'users/login'
+    end
+   end
+
+   post "/login" do
+     user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       redirect to '/home'
+      else
+       redirect to '/login'
+      end
+   end
+
+   get "/logout" do
+     if logged_in?
+      session.destroy
+      redirect to '/'
+     else
+      redirect to '/'
+     end
+   end
+
 
  end
